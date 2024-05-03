10267e566ee13b2aa9d295a5694d1b77c0c91bab
EnockCity <enockmwanzia17@gmail.com>
2024-05-03 23:47:14 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-# Define a resource class for managing pip packages
-class { 'pip' : }
-
-# Define a resource to install flask with a specific version
-package { 'python-flask':
-  ensure => '2.1.0',
+# installing flask using puppet
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
