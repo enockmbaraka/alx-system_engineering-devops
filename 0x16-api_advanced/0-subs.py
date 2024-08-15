0a6ac98eab50eb64dd7acf97f8d9f6a3dd423d7a
EnockCity <enockmwanzia17@gmail.com>
2024-08-16 00:27:07 +0300

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Function to query subscribers on a given Reddit subreddit."""
+""" Function that queries Reddit API , returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Return the total number of subscribers on subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Brendabett@alx-holbertonschool"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
