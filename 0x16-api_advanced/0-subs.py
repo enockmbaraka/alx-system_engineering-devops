459879dca7ca490ee69b3fa95240d885c5f75e51
EnockCity <enockmwanzia17@gmail.com>
2024-09-27 09:45:08 +0300

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "RedditSubscriberCountBot/1.0 (by /u/yourusername)"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        elif response.status_code == 404:
-            # Subreddit does not exist
-            return 0
-        else:
-            return 0
-    except requests.RequestException as e:
-        # Catch any request errors
+    headers = {
+        "User-Agent": "Brendabett@alx-holbertonschool"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
