660ef31943d66433b8e056a7cece832055026c56
EnockCity <enockmwanzia17@gmail.com>
2024-09-27 14:06:19 +0300

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,42 +1,22 @@
-#!usr/bin/python3
-"""Function to print top 10 posts for Reddit subreddit."""
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of first 10 posts listed for subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
-        "limit": 10  # Limit the response to 10 posts
+        "limit": 10
     }
-
-    # Making the GET request to the Reddit API
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    # Check for invalid subreddit or failed request
-    if response.status_code != 200:
-        print(None)
+    if response.status_code == 404:
+        print("None")
         return
-
-    try:
-        # Extracting data from JSON response
-        data = response.json().get("data", {})
-        children = data.get("children", [])
-
-        # Check if there are no hot posts
-        if not children:
-            print(None)
-            return
-
-        # Print the title of each hot post
-        for post in children:
-            print(post.get("data", {}).get("title"))
-
-    except ValueError:
-        # Handle cases where the JSON response is invalid
-        print(None)
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
